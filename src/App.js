1c6f92ec784ce135a7fac6f37db94b7589cc8b21
taichoup <taichoup@gmx.net>
2020-04-19 01:43:08 +0200

    remove remnants from Redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { _Store } from "./Store";
-import StoreContext from "./StoreContext";
 import { Results } from "./Results";
 import "./App.css";
 import data from "./data/astrodump.json";
@@ -8,7 +6,6 @@ import data from "./data/astrodump.json";
 function App() {
   const [sign, setSign] = useState("balance");
   const [category, setCategory] = useState("sante");
-  const [crawl, setCrawl] = useState(data);
   const [showResults, setShowResults] = useState(false);
 
   // function handleSignChange(event) {
@@ -24,75 +21,71 @@ function App() {
   }
 
   return (
-    <StoreContext.Consumer>
-      {(store) => (
-        <div className="App">
-          <header className="App-header">
-            <h1>Bienvenue sur Flandiulf, une expérience méta-astrologique.</h1>
-          </header>
-          <form id="signsForm" className="pure-form pure-form-aligned">
-            <div className="pure-control-group">
-              <label for="select_sign">Signe astrologique :</label>
-              <select id="select_sign" onChange={handleUserInputChange}>
-                <option value="balance">Balance</option>
-                <option value="scorpion">Scorpion</option>
-                <option value="gemeaux">G&eacute;meaux</option>
-                <option value="verseau">Verseau</option>
-                <option value="belier">B&eacute;lier</option>
-                <option value="taureau">Taureau</option>
-                <option value="lion">Lion</option>
-                <option value="cancer">Cancer</option>
-                <option value="sagittaire">Sagittaire</option>
-                <option value="capricorne">Capricorne</option>
-                <option value="poissons">Poissons</option>
-                <option value="vierge">Vierge</option>
-              </select>
-            </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue sur Flandiulf, une expérience méta-astrologique.</h1>
+      </header>
+      <form id="signsForm" className="pure-form pure-form-aligned">
+        <div className="pure-control-group">
+          <label for="select_sign">Signe astrologique :</label>
+          <select id="select_sign" onChange={handleUserInputChange}>
+            <option value="balance">Balance</option>
+            <option value="scorpion">Scorpion</option>
+            <option value="gemeaux">G&eacute;meaux</option>
+            <option value="verseau">Verseau</option>
+            <option value="belier">B&eacute;lier</option>
+            <option value="taureau">Taureau</option>
+            <option value="lion">Lion</option>
+            <option value="cancer">Cancer</option>
+            <option value="sagittaire">Sagittaire</option>
+            <option value="capricorne">Capricorne</option>
+            <option value="poissons">Poissons</option>
+            <option value="vierge">Vierge</option>
+          </select>
+        </div>
 
-            <div className="pure-control-group">
-              <label for="select_cat">Catégorie :</label>
-              <select id="select_cat" onChange={handleUserInputChange}>
-                <option value="sante">Sant&eacute;</option>
-                <option value="travail">Travail</option>
-                <option value="argent">Argent</option>
-                <option value="amour">Amour</option>
-              </select>
-            </div>
+        <div className="pure-control-group">
+          <label for="select_cat">Catégorie :</label>
+          <select id="select_cat" onChange={handleUserInputChange}>
+            <option value="sante">Sant&eacute;</option>
+            <option value="travail">Travail</option>
+            <option value="argent">Argent</option>
+            <option value="amour">Amour</option>
+          </select>
+        </div>
 
-            <div className="pure-controls">
-              <button
-                id="formButton"
-                type="button"
-                className="pure-button pure-button-primary"
-                onClick={() => {
-                  setSign(document.getElementById("select_sign").value);
-                  setCategory(document.getElementById("select_cat").value);
-                  setShowResults(true);
-                }}
-              >
-                Envoyer
-              </button>
-            </div>
-          </form>
+        <div className="pure-controls">
+          <button
+            id="formButton"
+            type="button"
+            className="pure-button pure-button-primary"
+            onClick={() => {
+              setSign(document.getElementById("select_sign").value);
+              setCategory(document.getElementById("select_cat").value);
+              setShowResults(true);
+            }}
+          >
+            Envoyer
+          </button>
+        </div>
+      </form>
 
-          {showResults ? (
-            <Results
-              crawl={crawl.filter(
-                (item) => item.sign === sign && item.category === category
-              )}
-              sign={sign}
-              category={category}
-            />
-          ) : null}
+      {showResults ? (
+        <Results
+          crawl={data.filter(
+            (item) => item.sign === sign && item.category === category
+          )}
+          sign={sign}
+          category={category}
+        />
+      ) : null}
 
-          {/* <button id="cloud-button" onclick="go_cloud();" class="btn btn-info">
+      {/* <button id="cloud-button" onclick="go_cloud();" class="btn btn-info">
             Generate tag cloud
           </button> */}
 
-          {/* <div id="cloud-wrapper"></div> */}
-        </div>
-      )}
-    </StoreContext.Consumer>
+      {/* <div id="cloud-wrapper"></div> */}
+    </div>
   );
 }
 
--- src/Results.js
+++ src/Results.js
@@ -1,27 +1,22 @@
 import React from "react";
-import StoreContext from "./StoreContext";
 
 export const Results = (props) => {
   return (
-    <StoreContext.Consumer>
-      {(store) => (
-        <div id="table-wrapper">
-          <table className="pure-table pure-table-horizontal">
-            {props.crawl
-              .filter(
-                (item) =>
-                  item.sign === props.sign && item.category === props.category
-              )
-              .map((r) => (
-                <tr>
-                  <td>{r.source}</td>
-                  <td>{r.timestamp}</td>
-                  <td>{r.content}</td>
-                </tr>
-              ))}
-          </table>
-        </div>
-      )}
-    </StoreContext.Consumer>
+    <div id="table-wrapper">
+      <table className="pure-table pure-table-horizontal">
+        {props.crawl
+          .filter(
+            (item) =>
+              item.sign === props.sign && item.category === props.category
+          )
+          .map((r) => (
+            <tr>
+              <td>{r.source}</td>
+              <td>{r.timestamp}</td>
+              <td>{r.content}</td>
+            </tr>
+          ))}
+      </table>
+    </div>
   );
 };
